4968ef75cab8875ac1e721c40e01ec698618a334
Stuart B. Wilkins <swilkins@bnl.gov>
2019-03-23 16:39:16 -0400

    Alpha version .. not tested on HW

--- iocBoot/iocSpecsEBE4/st.cmd
+++ iocBoot/iocSpecsEBE4/st.cmd
@@ -2,20 +2,22 @@
 
 < envPaths
 
+cd(${TOP})
+
 ## Register all support components
 dbLoadDatabase("${TOP}/dbd/specsEBE4.dbd")
 specsEBE4_registerRecordDeviceDriver pdbbase
 
 epicsEnvSet("EPICS_CA_AUTO_ADDR_LIST" , "NO")
 epicsEnvSet("EPICS_CA_ADDR_LIST"      , "10.23.0.255")
-epicsEnvSet("STREAM_PROTOCOL_PATH"    , "$(TOP)/protocols")
+epicsEnvSet("STREAM_PROTOCOL_PATH"    , "$(TOP)/protocol")
 
-drvAsynIPPortConfigure("EBE4", "10.23.3.11:7777")
+drvAsynIPPortConfigure("EBE4", "10.23.3.220:7777")
 #asynSetTraceMask("EBE4", 0, 0x9)
 #asynSetTraceIOMask("EBE4", 0, 0x2)
 
 ## Load record instances
-dbLoadRecord("$(TOP)/db/specsEBE4.db")
+dbLoadRecords("$(TOP)/db/specsEBE4.db", "Sys=XF:23ID2-ES,Name=Evap:1,PORT=EBE4")
 
 system("install -m 777 -d $(TOP)/as/save") 
 system("install -m 777 -d $(TOP)/as/req")
